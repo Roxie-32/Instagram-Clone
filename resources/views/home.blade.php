5d2d8528987dd0de313ff680c2f8aa81970b2dba
Roxie-32 <fizzyflexy397@gmail.com>
2020-11-22 12:50:59 +0100

    first

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <div class="row">
+       <div class="col-3 p-5">
+           <img src="/asset/2.png" alt="" class="rounded-circle ">
+       </div>
+       <div class="col-9 pt-5">
+           <div>
+               <h1 class="">freecodecamp</h1>
+           </div>
+           <div class="d-flex">
+               <div class="pr-5"><strong>212</strong>posts</div>
+               <div class="pr-5"><strong>212</strong>followers</div>
+               <div class="pr-5"><strong>212</strong>following</div>
+           </div>
+           <div>
+               <div class="pt-4 font-weight-bold">freecodecamp.org</div>
+               <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+               <div class="pt-4"><a href="">freecodecamp.org</a></div>
+           </div>
+       </div>
+   </div>
+   <div class="row pt-5">
+       <div class="col-4"><img src="/asset/3.jpg" class="w-100" alt=""></div>
+       <div class="col-4"><img src="/asset/3.jpg" class="w-100" alt=""></div>
+       <div class="col-4"><img src="/asset/3.jpg" class="w-100" alt=""></div>
+   </div>
+</div>
+@endsection
